cd04c4c275d6f76d964e61cc9188e45f1e6cf270
Hamid Zade <hamidmamadzade@gmail.com>
2016-07-13 19:37:08 +0100

    user can sign up

--- spec/features/01_users_feature_spec.rb
+++ spec/features/01_users_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'users' do
       expect(page).to have_link 'Sign Up'
     end
   end
+
+  scenario 'they should be able to sign up' do
+    visit '/'
+    click_link 'Sign Up'
+    fill_in :user_email, with: 'wade_wilson@test.com'
+    fill_in :user_password, with: 'deadpool'
+    fill_in :user_password_confirmation, with: 'deadpool'
+    click_button 'Sign up'
+
+    expect(page).to have_content 'wade_wilson@test.com'
+    expect(page).to have_content 'You have signed up successfully'
+  end
 end
