81a135771e5a1d0526f467119980c88acac20fb4
Hamid Zade <hamidmamadzade@gmail.com>
2016-07-15 12:51:27 +0100

    adds deleting posts feature test

--- spec/features/00_posts_features_spec.rb
+++ spec/features/00_posts_features_spec.rb
@@ -9,11 +9,12 @@ feature 'posts' do
     end
   end
 
-  context 'creating a post' do
-    context 'signed in user' do
-      before(:each) do
-        user = FactoryGirl.create(:user) ; login_as(user)  
-      end
+  context 'when signed in' do
+    before(:each) do
+      user = FactoryGirl.create(:user) ; login_as(user)  
+    end
+    
+    context 'creating a post' do
 
       scenario 'can create a new post' do
         visit '/posts'
@@ -27,13 +28,29 @@ feature 'posts' do
         expect(page).to have_content 'the finest'
       end
     end
+
+    context 'deleting post' do
+      scenario 'can delete a post' do
+        visit '/posts'
+        click_link 'Add a post'
+        attach_file :post_image, './spec/images/wilson_matt_n_peter.png'
+        fill_in :post_caption, with: 'the finest'
+        click_button 'Create Post'
+
+        click_button 'Delete Post'
+        expect(current_path).to eq posts_path
+        expect(page).to have_content 'Post deleted successfully'
+        expect(page).not_to have_css 'img[src*=\'wilson_matt_n_peter.png\']'
+        expect(page).not_to have_content 'the finest'
+      end
+    end
   end
 
   context 'when not signed in' do
     scenario 'cannot create a post' do
       visit '/posts'
       click_link 'Add a post'
-      
+
       expect(current_path).to eq new_user_session_path
       expect(page).to have_content 'You need to sign in or sign up'
     end
